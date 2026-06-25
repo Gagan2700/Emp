7b97b87c2784fe2ec4c7bb97ca7129d79900967c
Gagan Deep <deepgagan2700@gmail.com>
2026-06-25 23:52:54 +0530

    First Version-Employee Management api

---
+++ src/main/java/com/example/employee_management_api/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.example.employee_management_api.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.example.employee_management_api.model.Employee;
+
+@Service
+public class EmployeeService {
+
+    private List<Employee> employees = new ArrayList<>();
+
+    // Constructor to mock initial data
+    public EmployeeService() {
+        employees.add(new Employee(1, "Senthur", "DevOps", 50000));
+        employees.add(new Employee(2, "John", "Testing", 45000));
+    }
+
+    // Method to return all employees
+    public List<Employee> getAllEmployees() {
+        return employees;
+    }
+}
